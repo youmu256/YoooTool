73c588cb8862e46df6985bbd970ba1528bac1bb2
zzj <zzj@wooduan.com>
2019-04-17 20:50:07 +0800

    RandomPool

---
+++ YoooTool/Code/LevelCfg.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoooTool.Code
+{
+
+    public class RandomPool<T>
+    {
+        protected Dictionary<T,float> ObjectWeightMap = new Dictionary<T, float>();
+
+        protected Random SeedRandom;
+
+        protected float TotalWeight { get; set; }
+
+        public RandomPool(int seed = 0)
+        {
+            SeedRandom = new Random(seed);
+        }
+        public void SetItemWeight(T item, float weight)
+        {
+            if (!ObjectWeightMap.ContainsKey(item))
+            {
+                TotalWeight += weight;
+                ObjectWeightMap.Add(item,weight);
+                return;
+            }
+            TotalWeight += weight - ObjectWeightMap[item];
+            ObjectWeightMap[item] = weight;
+        }
+
+        public T GetItem()
+        {
+            float random = (float)SeedRandom.NextDouble() * TotalWeight;
+            float last = 0;
+            foreach (var pair in ObjectWeightMap)
+            {
+                if (random >= last && random < pair.Value + last)
+                {
+                    return pair.Key;
+                }
+                last = pair.Value;
+            }
+            return default(T);
+        }
+    }
+
+    public class PoolConstant
+    {
+        
+    }
+
+
+    public enum RoomType
+    {
+        Interation,
+        EnemyStrike,
+        KeepAlive,
+    }
+
+    public class InteractiveObject
+    {
+        public string Desc;
+
+    }
+
+    /// <summary>
+    /// interactive
+    /// </summary>
+    public class Room_Interactive
+    {
+        
+    }
+    /// <summary>
+    /// certain enemy battle
+    /// </summary>
+    public class Room_EnemyStrike
+    {
+
+    }
+    /// <summary>
+    /// keep alive in time
+    /// </summary>
+    public class Room_KeepAlive
+    {
+        public float Time;
+        
+    }
+
+    public class Level_Room
+    {
+
+    }
+
+
+    
+    public class LevelCfg 
+    {
+        
+    }
+}
---
+++ YoooTool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoooTool.Code;
+
+namespace YoooTool
+{
+    class Program
+    {
+        public class Item
+        {
+            public string Name;
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+            Random random = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(random.NextDouble()*100);
+            }
+            Console.ReadKey();
+            return;
+            */
+            RandomPool<Item> rp = new RandomPool<Item>();
+            rp.SetItemWeight(new Item() {Name = "5"}, 5);
+            rp.SetItemWeight(new Item() {Name = "10"}, 10);
+            Dictionary<Item,int> counter = new Dictionary<Item, int>();
+            for (int i = 0; i < 100000; i++)
+            {
+                var it = rp.GetItem();
+                if (!counter.ContainsKey(it))
+                {
+                    counter.Add(it,0);
+                }
+                counter[it]++;
+            }
+
+            foreach (var p in counter)
+            {
+                Console.WriteLine(p.Key.Name + "__" + p.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
